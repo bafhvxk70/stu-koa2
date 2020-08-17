3ec5c27be13833bd2ffc2af3189df02a99f079e4
chenchaoqiang <chenchaoqiang@58.com>
2020-08-17 20:00:37 +0800

    feat: 1

--- server/static/api/report.js
+++ server/static/api/report.js
@@ -3,10 +3,8 @@ var _body = document.getElementsByTagName('body')[0];
 var _s = Date.now()
 var ms = _s % 1000;
 var t = new Date();
-var _str = t.getHours() +"小时" + t.getMinutes() + "分钟" + t.getSeconds() + "秒" + ms +  "毫秒"
-// var element = document.createTextNode("我是script标签的时间:"+ _str);
-// _body.appendChild(element);
-document.getElementById('report').innerHTML = "我是script标签的时间" + _str
+var _str = t.getHours() +"小时" + t.getMinutes() + "分钟" + t.getSeconds() + "秒" + ms +  "毫秒";
+document.getElementById('report').innerHTML = "我是script标签的时间" + _str;
 (function() {
     var wb = document.createElement('script'),
         s = document.getElementsByTagName('head')[0],
