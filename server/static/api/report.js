a4551d0c4c9ba2e33f835adacd14719fc81d6576
chenchaoqiang <chenchaoqiang@58.com>
2020-08-18 20:44:40 +0800

    feat: 1

--- server/static/api/report.js
+++ server/static/api/report.js
@@ -1,13 +1,1 @@
-console.log('reportJs');
-
-(function() {
-  window._report1 = window.getT();
-  document.getElementById('report1').innerHTML = "我是script-native标签的时间---" + window._report1.s2;
-    var wb = document.createElement('script'),
-        s = document.getElementsByTagName('head')[0],
-        url = 'a.58cdn.com.cn/app58/rms/app/js/report1_32927.js?';
-    wb.type = 'text/javascript';
-    wb.async = false;
-    wb.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + url + (+new Date());
-    s.appendChild(wb);
-})();
+console.log("reportJs"),function(){window._report1=window.getT(),document.getElementById("report1").innerHTML="我是script-native标签的时间---"+window._report1.s2;var wb=document.createElement("script"),s=document.getElementsByTagName("head")[0];wb.type="text/javascript",wb.async=!1,wb.src=("https:"==document.location.protocol?"https://":"http://")+"a.58cdn.com.cn/app58/rms/app/js/report1_32927.js?"+ +new Date,s.appendChild(wb)}();
