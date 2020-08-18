94516386d31b180bf25d4c1ac73079edd7232ea2
chenchaoqiang <chenchaoqiang@58.com>
2020-08-18 14:34:47 +0800

    feat: add .

--- server/static/api/report.js
+++ server/static/api/report.js
@@ -1,11 +1,8 @@
 console.log('reportJs');
-var _body = document.getElementsByTagName('body')[0];
-var _s = Date.now()
-var ms = _s % 1000;
-var t = new Date();
-var _str = t.getHours() +"小时" + t.getMinutes() + "分钟" + t.getSeconds() + "秒" + ms +  "毫秒";
-document.getElementById('report1').innerHTML = "我是script-native标签的时间---" + _str;
+
 (function() {
+  window._report1 = window.getT();
+  document.getElementById('report1').innerHTML = "我是script-native标签的时间---" + window._report1.s2;
     var wb = document.createElement('script'),
         s = document.getElementsByTagName('head')[0],
         url = 'a.58cdn.com.cn/app58/rms/app/js/report1_32927.js?';
