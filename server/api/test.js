3fea899f61abfb88dbf4424a0b76376eb0c6aab6
chenchaoqiang <chenchaoqiang@58.com>
2020-08-19 17:51:30 +0800

    feat: 接口

--- server/api/test.js
+++ server/api/test.js
@@ -1,6 +1,12 @@
 const Router = require('koa-router');
 const log4js = require("log4js");
 const router = new Router();
+log4js.configure({
+  appenders: { cheese: { type: "file", filename: "cheese.log" } },
+  categories: { default: { appenders: ["cheese"], level: "error" } }
+});
+
+const logger = log4js.getLogger("cheese");
 router
     .get('/list/', (ctx, next) => {
         ctx.body = '1111';
--- server/app.js
+++ server/app.js
@@ -2,16 +2,18 @@ const Koa = require('koa');
 const static = require('koa-static');
 const app = new Koa();
 const router = require('./api/test')
-app.use(static(__dirname+'/static'), {
-  setHeadears: (res,path,stats) => {
-    res.setHeader('Cache-Control',['private','max-age=60000'])
-  }
-})
-app.use(async (ctx, next) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+
+// {
+//   setHeadears: (res,path,stats) => {
+//     res.setHeader('Cache-Control',['private','max-age=60000'])
+//   }
+// }
+app.use(static(__dirname+'/static'))
+// app.use(async (ctx, next) => {
+//     const start = Date.now();
+//     await next();
+//     const ms = Date.now() - start;
+//     console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+// });
 app.use(router.routes())
 app.listen(3000);
