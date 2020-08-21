cd061454dc7fa387fca9cc5d47840a2c98921de7
chenchaoqiang <chenchaoqiang@58.com>
2020-08-21 11:21:40 +0800

    feat: 1

--- server/app.js
+++ server/app.js
@@ -3,15 +3,11 @@ const static = require('koa-static');
 const app = new Koa();
 const router = require('./api/test')
 
-// {
-//   setHeadears: (res,path,stats) => {
-//     res.setHeader('Cache-Control',['private','max-age=60000'])
-//   }
-// }
-app.use(static(__dirname+'/static', {
-  maxage: 1110000
-}))
-// app.use(static(__dirname+'/static'))
+
+// app.use(static(__dirname+'/static', {
+//   maxage: 1110000
+// }))
+app.use(static(__dirname+'/static'))
 // app.use(async (ctx, next) => {
 //     const start = Date.now();
 //     await next();
